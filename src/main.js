508ab4723be22b0d4b62bb7be9494bda0248438d
codewxh <914067438@qq.com>
2021-11-30 20:02:19 +0800

    gagaui初始化

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import GaButton from './components/button.vue'
+
 Vue.config.productionTip = false
+
+Vue.component(GaButton.name, GaButton)
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <GaButton>登录</GaButton>
+    <GaButton>注册</GaButton>
+    <GaButton>我的</GaButton>
+    <GaButton>点击</GaButton>
   </div>
 </template>
